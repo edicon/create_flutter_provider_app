af33973e6a019bdf9a64f7e2c096f2dda2f262b7
EDICON <hslee.edicon@gmail.com>
2020-04-29 10:10:53 +0900

    -fix firebase_core version:0.4.4+3

--- lib/services/firebase_analytics.dart
+++ lib/services/firebase_analytics.dart
@@ -1,3 +1,9 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics();
+final FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
+// Usage:
+// import 'firebase_analytics.dart';
+// analytics.logLogin();
